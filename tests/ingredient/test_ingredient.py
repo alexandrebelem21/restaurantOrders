f5dbe18eb911d6037ef676e2e531d081bf248d25
alexandrebelem21 <alexandrebelem21@gmail.com>
2023-07-17 05:59:09 -0300

    Req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,13 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    assert hash(Ingredient("queijo mussarela")) == hash(
+        Ingredient("queijo mussarela"))
+    assert hash(Ingredient("queijo mussarela")) != hash(Ingredient("presunto"))
+    assert Ingredient("queijo mussarela") == Ingredient("queijo mussarela")
+    assert Ingredient("queijo mussarela") != Ingredient("presunto")
+    assert repr(Ingredient("cebola")) == "Ingredient('cebola')"
+    assert Ingredient("cebola").name == "cebola"
+    assert Ingredient("bacon").restrictions != {Restriction.GLUTEN}
+    assert set(Ingredient("farinha").restrictions) == {Restriction.GLUTEN}
